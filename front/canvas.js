8d2cc768e9862152b90eca34abbb5fa442e1613f
HenrickyL <HenrickyL1@gmail.com>
2023-12-10 15:57:04 -0300

    style

--- front/canvas.js
+++ front/canvas.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', ()=>{
     const canvas = document.querySelector('canvas')
-    ResizeFull(canvas)
+    Resize(canvas)
     const context = canvas.getContext('2d')
     context.fillRect(10,10, 20,20)
 })
@@ -10,4 +10,8 @@ window.addEventListener('load', ()=>{
 function ResizeFull(canvas){
     canvas.width = window.innerWidth*0.99
     canvas.height = window.innerHeight*0.99
+}
+function Resize(canvas){
+    canvas.width = 800
+    canvas.height = 600
 }
